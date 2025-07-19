90eeaf535de7d3b79accf5dadd8f5fb1c7ed97b2
root <root@459478-yj360.in>
2025-07-19 15:45:36 +0530

    kefnefhefhef

--- src/Pages/Login/LoginPage.jsx
+++ src/Pages/Login/LoginPage.jsx
@@ -94,7 +94,7 @@ function LoginPage() {
   return (
     <Box sx={{ mb: 4, mt: 3, display: "flex", flexDirection: "column" }}>
       <Box sx={{ mb: 3, display: "flex", flexDirection: "column" }}>
-        <Typography variant="heading">hi WELCOME TO Dipath</Typography>
+        <Typography variant="heading">hi WELCOME TO Dipath By tripath logisticSSSSSS</Typography>
         <Typography variant="bodySmall" color="text.secondary">
           Version 6.8H
         </Typography>
